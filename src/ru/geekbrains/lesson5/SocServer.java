d010ecd9d4b8ee3ab99aaa0467a1d404e1cdac57
psylon <stanislav.antunovich@yandex.ru>
2019-04-18 23:25:27 +0300

    1. Написать консольный вариант клиент\серверного приложения, в котором пользователь может писать сообщения, как на клиентской стороне, так и на серверной. Т.е. если на клиентской стороне написать «Привет», нажать Enter, то сообщение должно передаться на сервер и там отпечататься в консоли. Если сделать то же самое на серверной стороне, то сообщение передается клиенту и печатается у него в консоли. Есть одна особенность, которую нужно учитывать: клиент или сервер может написать несколько сообщений подряд. Такую ситуацию необходимо корректно обработать. Разобраться с кодом с занятия: он является фундаментом проекта-чата *ВАЖНО! * Сервер общается только с одним клиентом, т.е. не нужно запускать цикл, который будет ожидать второго/третьего/n-го клиентов.

---
+++ src/ru/geekbrains/lesson5/SocServer.java
@@ -0,0 +1,74 @@
+package ru.geekbrains.lesson5;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SocServer {
+
+    private static final int PORT = 8888;
+    private static boolean online;
+
+    private static void startReading(Socket socket) throws IOException {
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+
+        new Thread(() -> {
+            while (online) {
+                try {
+                    System.out.println("New message: " + in.readUTF());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    online = false;
+                }
+            }
+        }).start();
+
+    }
+
+    private static void startWriting(Socket socket) throws IOException {
+
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        Scanner scanner = new Scanner(System.in);
+
+        new Thread(() -> {
+            String message;
+            do {
+                System.out.print("Input message > ");
+                message = scanner.nextLine();
+
+                if (message.equals("/stop")) {
+                    online = false;
+                    break;
+                }
+                try {
+                    out.writeUTF(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    online = false;
+                }
+            } while (online && scanner.hasNextLine());
+        }).start();
+    }
+
+    public static void main(String[] args) {
+        online = true;
+
+        try {
+
+            ServerSocket serverSocket = new ServerSocket(PORT);
+
+            System.out.println("Server waiting for connection");
+
+                Socket socket = serverSocket.accept();
+                startReading(socket);
+                startWriting(socket);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
